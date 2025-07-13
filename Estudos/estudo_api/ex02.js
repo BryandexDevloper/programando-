28054a3fc4aadd6b30d06fe3ed63989a289774d7
BryandexDevloper <adrianbryan072@gmail.com>
2025-07-13 12:43:48 -0300

    gg

---
+++ Estudos/estudo_api/ex02.js
@@ -0,0 +1,55 @@
+
+let guardar_Pecoas = []
+
+class Cadastro{
+
+    static GerarCpf(){
+
+        let cpf = Math.floor(Math.random() * 3000)
+        
+        if(guardar_Pecoas.some(res => res.cpf === cpf)){
+
+            return Cadastro.GerarCpf()
+        }
+
+
+        return cpf
+       
+    }
+
+    constructor(name,user,email,phone){
+
+            this.name = name
+            this.user = user
+            this.email = email
+            this.phone = phone
+            this.cpf = Cadastro.GerarCpf()
+    }
+}
+
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(resposta => resposta.json())
+.then(dados =>{
+try{
+
+
+    dados.forEach((res)=>{
+
+        let pessoa = new Cadastro(res.name,res.username,res.email,res.phone)
+        guardar_Pecoas.push(pessoa)
+    })
+
+    
+
+
+}catch(error){
+    console.log("Descolpe ocorreu um erro" + error)
+}
+})
+
+
+console.log(guardar_Pecoas)
