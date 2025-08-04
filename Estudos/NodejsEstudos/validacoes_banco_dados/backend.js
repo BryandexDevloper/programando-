1331cefbbd826cf1521f33e86c26b2e771e58852
BryandexDevloper <adrianbryan072@gmail.com>
2025-08-04 11:34:18 -0300

    gg

--- Estudos/NodejsEstudos/validacoes_banco_dados/backend.js
+++ Estudos/NodejsEstudos/validacoes_banco_dados/backend.js
@@ -1,57 +1,161 @@
 const express = require('express')
 const mysql = require('mysql2')
+const cors = require('cors')
 const porta = process.env.PORT
+const nodemailer = require('nodemailer')
 const servidor = express()
+
+
+servidor.use(cors())
 servidor.use(express.json())
 
-const conexao = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'loja'
+
+const envio_de_email = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'adrianbryan072@gmail.com',
+        pass: 'fnfb gmzw sefz gaem'
+    }
 })
 
-servidor.post('/login',(req,res)=>{
-    conexao.query('SELECT * FROM usuarios',(error,resultado)=>{
-        const {email,senha} = req.body
-        if(error){
-           return res.status(400).json({mensagem:'Ocorreu algum erro inesperado'})
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'loja'
+})
+
+servidor.post('/login', (req, res) => {
+    conexao.query('SELECT * FROM usuarios', (error, resultado) => {
+        const { email, senha } = req.body
+        if (error) {
+            return res.status(400).json({ mensagem: 'Ocorreu algum erro inesperado' })
         }
 
-        const usuarioEncontrado = resultado.find(res =>res.email === email && res.senha === senha)
-        if(usuarioEncontrado){
-            return res.status(200).json({mensagem:'Usuario logado com sucesso'})
-        }else{
-            return res.status(400).json({mensagem:'Credenciais incorretas'})
+        const usuarioEncontrado = resultado.find(res => res.email === email && res.senha === senha)
+        if (usuarioEncontrado) {
+            return res.status(200).json({ mensagem: 'Usuario logado com sucesso' })
+        } else {
+            return res.status(400).json({ mensagem: 'Credenciais incorretas' })
         }
     })
-    
+
 
 })
 
 
-servidor.post('/cadastro',(req,res)=>{
-    conexao.query('SELECT * FROM usuarios',(error,resultado)=>{
-        const {email,senha} = req.body
-        if(error){
-            return res.status(400).json({mensagem:'Ocorreu algum erro inesperado'})
+servidor.post('/cadastro', (req, res) => {
+    conexao.query('SELECT * FROM usuarios', (error, resultado) => {
+        const { email, senha } = req.body
+        if (error) {
+            return res.status(400).json({ mensagem: 'Ocorreu algum erro inesperado' })
         }
 
         const usuarioEncontrado = resultado.find(res => res.email === email)
 
-        if(usuarioEncontrado){
-            return res.status(403).json({mensagem:'Esse usuario ja existe faça login ou tente outro email'})
-        }else{
-            conexao.query('INSERT INTO usuarios (email,senha) VALUES (?,?)',[email,senha],(error,resultado1)=>{
-                if(error){
-                    return res.status(400).json({mensagem:'Ocorreu algum erro inesperado'})
+        if (usuarioEncontrado) {
+            return res.status(403).json({ mensagem: 'Esse usuario ja existe faça login ou tente outro email' })
+        } else {
+            conexao.query('INSERT INTO usuarios (email,senha) VALUES (?,?)', [email, senha], (error, resultado1) => {
+                if (error) {
+                    return res.status(400).json({ mensagem: 'Ocorreu algum erro inesperado' })
                 }
 
-                res.status(200).json({mensagem:'Usuario cadastrado com exito'})
+                res.status(200).json({ mensagem: 'Usuario cadastrado com exito' })
             })
         }
     })
 })
 
 
-servidor.listen(porta||3000,()=>{console.log('Servidor Rodando...')})
+servidor.post('/recuperarsenha', async (req, res) => {
+
+    const { email } = req.body;
+
+    conexao.query('SELECT * FROM usuarios', async (error, resultado) => {
+        if (error) {
+            return res.status(400).json({ mensagem: 'Desculpe, algo deu errado' });
+        }
+
+        const Usuario_encontrado = resultado.find(user => user.email === email);
+        if (!Usuario_encontrado) {
+            return res.status(404).json({ mensagem: 'Usuário não encontrado' });
+        }
+
+        const codigo = Math.floor(Math.random() * 10000);
+        const id = Usuario_encontrado.id;
+
+        conexao.query('UPDATE usuarios SET codigo_verificacao = ? WHERE id = ?', [codigo, id], async (erroAtualizar) => {
+            if (erroAtualizar) {
+                return res.status(400).json({ mensagem: 'Erro ao atualizar o código' });
+            }
+
+            try {
+                await envio_de_email.sendMail({
+                    from: 'adrianbryan072@gmail.com',
+                    to: email,
+                    subject: 'Código de verificação para redefinição de senha',
+                    html: `<h1>Olá, ${Usuario_encontrado.email}</h1><p>Seu código de verificação é: <strong>${codigo}</strong></p>`
+                });
+
+                res.status(200).json({ mensagem: 'Código enviado para o e-mail', email: email });
+            } catch (err) {
+                res.status(500).json({ mensagem: 'Erro ao enviar o e-mail', erro: err });
+            }
+        });
+    });
+
+
+});
+
+servidor.put('/validarcodigo', (req, res) => {
+    const { email, passcode, nova_senha } = req.body
+
+    conexao.query('SELECT * FROM usuarios',async (error, resposta) => {
+        if (error) {
+            return res.status(500).json({ mensagem: 'Erro ao buscar usuários' })
+        }
+
+        const Usuario_encontrado = resposta.find(res => res.email === email)
+        if (!Usuario_encontrado) {
+            return res.status(404).json({ mensagem: 'Usuário não encontrado' })
+        }
+
+        const codigoConvertido = Number(passcode)
+        if (isNaN(codigoConvertido)) {
+            return res.status(400).json({ mensagem: 'Código inválido' })
+        }
+
+        if (Usuario_encontrado.codigo_verificacao !== codigoConvertido) {
+            return res.status(400).json({ mensagem: 'Código incorreto' })
+        }
+
+        conexao.query('UPDATE usuarios SET senha = ? WHERE id = ?', [nova_senha, Usuario_encontrado.id], (error1) => {
+            if (error1) {
+                return res.status(500).json({ mensagem: 'Erro ao atualizar senha' })
+            }
+            
+            conexao.query('UPDATE usuarios SET codigo_verificacao = NULL WHERE id = ?',[Usuario_encontrado.id],(erro)=>{
+                if(erro){
+                    return res.status(500).json({mensagem:'Erro inesperado'})
+                }
+            })
+            
+            return res.status(200).json({ mensagem: 'Senha atualizada com sucesso' })
+        })
+        
+        try{
+            await envio_de_email.sendMail({
+                    from: 'adrianbryan072@gmail.com',
+                    to: email,
+                    subject: 'Sua senha foi redefinida',
+                    html: `<h1>Olá, ${Usuario_encontrado.email}</h1><p>Sua senha acabou de ser redenifinida</p>`
+            })
+        }catch(err){
+            res.status(500).json({mensagem:'Erro inesperado',erro:err})
+        }
+    })
+})
+
+
+servidor.listen(porta || 3000, () => { console.log('Servidor Rodando...') })
--- Estudos/NodejsEstudos/validacoes_banco_dados/funcoes.js
+++ Estudos/NodejsEstudos/validacoes_banco_dados/funcoes.js
@@ -1,29 +1,29 @@
 const main = document.querySelector(".container_conteudo")
 
-function Telalogin(){
-    main.innerHTML=""
-    const form =  document.createElement("div")
+function Telalogin() {
+    main.innerHTML = ""
+    const form = document.createElement("div")
     form.classList.add('form')
     main.appendChild(form)
 
     const logintext = document.createElement("h3")
     logintext.classList.add('logintext')
-    logintext.setAttribute('id','logintext')
-    logintext.textContent=`Login`
+    logintext.setAttribute('id', 'logintext')
+    logintext.textContent = `Login`
     form.appendChild(logintext)
 
     const container_input_email = document.createElement('div')
     container_input_email.classList.add('container_input')
-    container_input_email.innerHTML=`<label for="email">Email</label>
+    container_input_email.innerHTML = `<label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder='Digite seu email'>`
 
     form.appendChild(container_input_email)
 
 
-   
+
     const container_input_senha = document.createElement('div')
     container_input_senha.classList.add('container_input')
-    container_input_senha.innerHTML=`<label for="senha">Senha</label>
+    container_input_senha.innerHTML = `<label for="senha">Senha</label>
             <input type="email" name="senha" id="senha" placeholder='Digite sua senha'>`
 
     form.appendChild(container_input_senha)
@@ -31,11 +31,11 @@ function Telalogin(){
     const container_opcoes = document.createElement("div")
     container_opcoes.classList.add('container_opcoes')
     form.appendChild(container_opcoes)
-    container_opcoes.innerHTML=`<p class="btn_esqueci_senha">Esqueceu sua senha?</p><p class="btn_cadastrar-se">Cadastrar-se</p>`
-    
+    container_opcoes.innerHTML = `<p class="btn_esqueci_senha">Esqueceu sua senha?</p><p class="btn_cadastrar-se">Cadastrar-se</p>`
+
     const btn_login = document.createElement("div")
     btn_login.classList.add('btn_login')
-    btn_login.textContent=`Logar`
+    btn_login.textContent = `Logar`
     form.appendChild(btn_login)
 
     const esqueci_senha = document.querySelector(".btn_esqueci_senha")
@@ -44,37 +44,37 @@ function Telalogin(){
     const email = document.querySelector("#email")
     const senha = document.querySelector("#senha")
 
-    btn_login.addEventListener("click",()=>{
-        Login(email.value,senha.value)
-        email.value=""
-        senha.value=""
+    btn_login.addEventListener("click", () => {
+        Login(email.value, senha.value)
+        email.value = ""
+        senha.value = ""
     })
 
-    esqueci_senha.addEventListener("click",()=>{
+    esqueci_senha.addEventListener("click", () => {
         Telacadastro()
     })
 
-    esqueci_senha.addEventListener("click",()=>{
-        Telacadastro()
+    esqueci_senha.addEventListener("click", () => {
+        RecuperarSenha()
     })
 }
 
 
-function Telacadastro(){
-    main.innerHTML=""
-    const form =  document.createElement("div")
+function Telacadastro() {
+    main.innerHTML = ""
+    const form = document.createElement("div")
     form.classList.add('form')
     main.appendChild(form)
 
     const logintext = document.createElement("h3")
     logintext.classList.add('logintext')
-    logintext.setAttribute('id','logintext')
-    logintext.textContent=`Cadastrar-se`
+    logintext.setAttribute('id', 'logintext')
+    logintext.textContent = `Cadastrar-se`
     form.appendChild(logintext)
 
     const container_input_email = document.createElement('div')
     container_input_email.classList.add('container_input')
-    container_input_email.innerHTML=`<label for="email">Email</label>
+    container_input_email.innerHTML = `<label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder='Digite seu email'>
             <label for="confirmar_email">Confirmar Email</label>
             <input type="email" name="confirmar_email" id="confirmar_email" placeholder='Confirme seu email'>`
@@ -82,10 +82,10 @@ function Telacadastro(){
     form.appendChild(container_input_email)
 
 
-   
+
     const container_input_senha = document.createElement('div')
     container_input_senha.classList.add('container_input')
-    container_input_senha.innerHTML=`<label for="senha">Senha</label>
+    container_input_senha.innerHTML = `<label for="senha">Senha</label>
             <input type="email" name="senha" id="senha" placeholder='Digite sua senha'>`
 
     form.appendChild(container_input_senha)
@@ -93,69 +93,221 @@ function Telacadastro(){
     const container_opcoes = document.createElement("div")
     container_opcoes.classList.add('container_opcoes')
     form.appendChild(container_opcoes)
-    container_opcoes.innerHTML=`<p class="btn_fazer_login">Fazer login</p><p class="btn_recuperar_senha">Recuperar senha</p>`
-    
+    container_opcoes.innerHTML = `<p class="btn_fazer_login">Fazer login</p><p class="btn_recuperar_senha">Recuperar senha</p>`
+
     const btn_login = document.createElement("div")
     btn_login.classList.add('btn_login')
-    btn_login.textContent=`Cadastrar-se`
+    btn_login.textContent = `Cadastrar-se`
     form.appendChild(btn_login)
 
     const btn_recuperar_senha = document.querySelector(".btn_recuperar_senha")
     const btn_fazer_login = document.querySelector(".btn_fazer_login")
 
-    btn_fazer_login.addEventListener("click",()=>{
+    btn_fazer_login.addEventListener("click", () => {
         Telalogin()
     })
 
-    btn_recuperar_senha.addEventListener("click",()=>{
-        Telalogin()
+    btn_recuperar_senha.addEventListener("click", () => {
+        RecuperarSenha()
     })
 
     const input_email = document.querySelector("#email")
     const input_confirmar_email = document.querySelector("#confirmar_email")
     const input_senha = document.querySelector("#senha")
 
-    btn_login.addEventListener("click",()=>{
-        Cadastro(input_email.value,input_confirmar_email.value,input_senha.value)
-        input_email.value=""
-        input_senha.value=""
-        input_confirmar_email.value=""
+    btn_login.addEventListener("click", () => {
+        Cadastro(input_email.value, input_confirmar_email.value, input_senha.value)
+        input_email.value = ""
+        input_senha.value = ""
+        input_confirmar_email.value = ""
     })
 }
 
-function Mostrarinformacoes(){
+function RecuperarSenha() {
+    main.innerHTML = ""
+    const form = document.createElement("div")
+    form.classList.add('form')
+    main.appendChild(form)
+
+    const logintext = document.createElement("h3")
+    logintext.classList.add('logintext')
+    logintext.setAttribute('id', 'logintext')
+    logintext.textContent = `Recuperação de senha`
+    form.appendChild(logintext)
+
+    const container_input_email = document.createElement('div')
+    container_input_email.classList.add('container_input')
+    container_input_email.innerHTML = `<label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder='Digite seu email'>`
+
+    form.appendChild(container_input_email)
+
+
+
+    const container_opcoes = document.createElement("div")
+    container_opcoes.classList.add('container_opcoes')
+    form.appendChild(container_opcoes)
+    container_opcoes.innerHTML = `<p class="btn_esqueci_senha">Fazer Login</p><p class="btn_cadastrar-se">Cadastrar-se</p>`
+
+    const btn_login = document.createElement("div")
+    btn_login.classList.add('btn_login')
+    btn_login.textContent = `Recuperar Senha`
+    form.appendChild(btn_login)
+
+    const esqueci_senha = document.querySelector(".btn_esqueci_senha")
+    const btn_cadastrar = document.querySelector(".btn_cadastrar-se")
+
+    const email = document.querySelector("#email")
+
+
+    btn_login.addEventListener("click", () => {
+        Recupearsenha(email.value)
+        email.value = ""
+
+    })
+
+    esqueci_senha.addEventListener("click", () => {
+        Login()
+    })
+
+    btn_cadastrar.addEventListener("click", () => {
+        Telacadastro()
+    })
+}
+
+function ValidarCodigo(email) {
+    main.innerHTML = ""
+    const form = document.createElement("div")
+    form.classList.add('form')
+    main.appendChild(form)
+
+    const logintext = document.createElement("h3")
+    logintext.classList.add('logintext')
+    logintext.setAttribute('id', 'logintext')
+    logintext.textContent = `Validar codigo`
+    form.appendChild(logintext)
+
+    const container_input_email = document.createElement('div')
+    container_input_email.classList.add('container_input')
+    container_input_email.innerHTML = `<label for="ValidarCodigo">Validação de codigo</label>
+            <input type="email" name="ValidarCodigo" id="ValidarCodigo" placeholder='Digite seu Codigo de verificação'>
+            <input type="Password" name="novasenha" id="novasenha" placeholder="Digite sua nova senha"> `
+
+    form.appendChild(container_input_email)
+
+
+
+    const container_opcoes = document.createElement("div")
+    container_opcoes.classList.add('container_opcoes')
+    form.appendChild(container_opcoes)
+    container_opcoes.innerHTML = `<p class="btn_esqueci_senha">Fazer Login</p><p class="btn_cadastrar-se">Cadastrar-se</p>`
+
+    const btn_login = document.createElement("div")
+    btn_login.classList.add('btn_login')
+    btn_login.textContent = `Recuperar Senha`
+    form.appendChild(btn_login)
+
+    const esqueci_senha = document.querySelector(".btn_esqueci_senha")
+    const btn_cadastrar = document.querySelector(".btn_cadastrar-se")
+
+    const ValidarCodigo = document.querySelector("#ValidarCodigo")
+    const novasenha = document.querySelector("#novasenha")
+
+    btn_login.addEventListener("click", () => {
+        validarcodigo(email, ValidarCodigo.value, novasenha.value)
+        ValidarCodigo.value = ""
+        novasenha.value = ""
+    })
+
+    esqueci_senha.addEventListener("click", () => {
+        Login()
+    })
+
+    btn_cadastrar.addEventListener("click", () => {
+        Telacadastro()
+    })
+}
+
+function Mostrarinformacoes() {
 
 }
 
-async function Login(email,senha) {
-    const data = await fetch('http://localhost:3000/login',{
-        method:'POST',
-        headers:{'Content-type':'application/json'},
-        body:JSON.stringify({
-            email:email,
-            senha:senha
+async function Login(email, senha) {
+    const data = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            email: email,
+            senha: senha
         })
     })
 
     const resultado = await data.json()
     const texto = document.querySelector("#logintext")
-    texto.textContent=`${resultado.mensagem}`
+    texto.textContent = `${resultado.mensagem}`
 }
 
-async function Cadastro(email,confirmar_email,senha) {
-    const data = await fetch('http://localhost:3000/cadastro',{
-        method:'POST',
-        headers:{'Content-type':'application/json'},
-        body:JSON.stringify({
-            email:email,
-            confirmar_email:confirmar_email,
-            senha:senha
+async function Cadastro(email, confirmar_email, senha) {
+    const data = await fetch('http://localhost:3000/cadastro', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            email: email,
+            confirmar_email: confirmar_email,
+            senha: senha
         })
     })
-    
+
     const resultado = await data.json()
     const texto = document.querySelector("#logintext")
-    texto.textContent=`${resultado.mensagem}`
+    texto.textContent = `${resultado.mensagem}`
+}
+async function validarcodigo(email, ValidarCodigo, novasenha) {
+    try {
+        const data = await fetch('http://localhost:3000/validarcodigo', {
+            method: 'PUT',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                email: email,
+                passcode: ValidarCodigo,
+                nova_senha: novasenha
+            })
+        })
+
+        const resultado = await data.json()
+        const texto = document.querySelector("#logintext")
+        texto.textContent = resultado.mensagem
+    } catch (error) {
+        const texto = document.querySelector("#logintext")
+        texto.textContent = "Erro inesperado ao validar código"
+        console.error(error)
+    }
+}
+
+
+
+async function Recupearsenha(email) {
+    const data = await fetch('http://localhost:3000/recuperarsenha', {
+        method: 'POST',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({
+            email: email
+        })
+    })
+
+    const resultado = await data.json()
+    const texto = document.querySelector("#logintext")
+    texto.textContent = `${resultado.mensagem}`
+
+    const id = setTimeout(() => {
+
+        if (resultado) {
+            ValidarCodigo(resultado.email)
+        } else {
+            texto.textContent = `${resultado.mensagem}`
+            clearTimeout(id)
+        }
+    }, 4000)
 }
 
 Telalogin()
