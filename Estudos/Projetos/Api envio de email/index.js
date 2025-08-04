c4916cc06d052f9db1a91224560923ab697225ec
BryandexDevloper <adrianbryan072@gmail.com>
2025-08-04 13:16:27 -0300

    gg

---
+++ Estudos/Projetos/Api envio de email/index.js
@@ -0,0 +1,88 @@
+
+
+
+const express = require("express");
+const cors = require("cors");
+const transponder = require("nodemailer");
+const port = process.env.PORT || 3000;
+
+const servidor = express();
+servidor.use(cors());
+servidor.use(express.json());
+
+servidor.post("/enviar_mail",async (req, res) => {
+  const { service, email, password, to, subject,html,email_user } = req.body;
+
+  if(!service || !email || !password || !to || !subject || !html || !email_user){
+    return res.status(400).json({mensagem:"Ainda falta dados importantes para o envio do email"})
+  }
+
+  try {
+    const envio_mail = transponder.createTransport({
+      service: service,
+      auth: {
+        user: email,
+        pass: password,
+      },
+    });
+
+    await envio_mail.sendMail({
+      from: email,
+      to: email_user,
+      subject: subject,
+      html: html
+    });
+
+    return res.status(200).json({mensagem:"Email enviado com sucesso"});
+  } catch (error) {
+    console.error("Erro ao enviar email:", error);
+    return res.status(500).json({mensagem:"Erro ao enviar email", erro: error.message});
+  }
+  
+
+  
+});
+
+const figlet = require("figlet");
+const chalk = require("chalk")
+
+figlet("BRYANDEX    DEVLOPER",{
+  font:"Slant",
+  horizontalLayout:"default",
+  verticalLayout:"default"
+},(err,data)=>{
+  if(err){
+    console.log(err)
+    return;
+  }
+
+  if(!data){
+    console.log("Sem dados de texto")
+    return;
+  }
+
+  console.log(chalk.green(data))
+})
+
+
+figlet("API  ENVIO  DE  MAILS",{
+  font:"Standard",
+  horizontalLayout:"default",
+  verticalLayout:"default"
+},(err,data)=>{
+  if(err){
+    console.log(err)
+    return;
+  }
+
+  if(!data){
+    console.log("Sem dados de texto")
+    return;
+  }
+
+  console.log(chalk.red(data))
+})
+
+servidor.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
